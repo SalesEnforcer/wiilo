c33da8d97f04c92032b918d2ac9422fd505f46f7
Sales Enforcer <eneh.bright@salesenforcer.com.ng>
2026-07-07 06:26:01 +0100

    added mobile view blocker

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+﻿import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common'; // Import CommonModule
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet], // Added CommonModule
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'wiilo';
+  isMobile = false; // Flag to check if mobile device
+
+  ngOnInit() {
+    // Detect mobile browsers, iOS, and Android viewports
+    this.isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+  }
 }
